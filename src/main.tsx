15ed4f20d550b5393dbe213cfe4add937424054f
Hugo Luna <hugo94ronal@gmail.com>
2022-05-28 22:32:54 -0500

    Setup router,path alias setup,favicon and app title  update

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./app/App";
+import { BrowserRouter } from "react-router-dom";
+
+import App from "~app/App";
 
 import "./index.css";
 import "./reset.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
